c46218b1850a64e7dd4a44f949be3c8f0d5fceec
pyh <alanpeng@thetiger.com.cn>
2018-01-17 22:02:03 +0800

    配置 数据库连接

--- App/Tool/MysqlTool.py
+++ App/Tool/MysqlTool.py
@@ -15,3 +15,25 @@ class MysqlTool:
         db = config.get_config_value('mysql.db.db')
         self.__db = pymysql.connect(host=host, user=user, password=password, port=port, db=db)
         self.__cursor = self.__db.cursor()
+
+    def insert(self, table, data):
+        """
+        插入数据
+        :param table:表名
+        :param data:插入数据
+        :return:
+        """
+        sql = 'insert into ' + table + '('
+        keys = data.keys()
+        key_string = ','.join(keys)
+        sql = sql + key_string + ')'
+        try:
+            self.__cursor.execute(sql,data.values())
+            self.__db.commit()
+        except:
+            self.__db.rollback()
+
+
+
+
+
